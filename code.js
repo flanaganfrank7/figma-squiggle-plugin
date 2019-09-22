7468c2727ea4d31827cd92a0aa3bc87e030f74c5
frank <frankflanagan17@gmail.com>
2019-09-21 20:26:15 -0400

    for approval

--- code.js
+++ code.js
@@ -1,66 +1,78 @@
-// This plugin will open a modal to prompt the user to enter a number, and
-// it will then create that many rectangles on the screen.
-// This file holds the main code for the plugins. It has access to the *document*.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser enviroment (see documentation).
-// This shows the HTML page in "ui.html".
+// Show the UI modal on running the plugin
 figma.showUI(__html__);
-figma.ui.resize(300, 300)
-// Calls to "parent.postMessage" from within the HTML page will trigger this
-// callback. The callback will be passed the "pluginMessage" property of the
-// posted message.
+// Resize the modal to 300x240
+figma.ui.resize(300, 240);
+
+// Interpret the message that we're sending over from the click event in the UI
 figma.ui.onmessage = msg => {
-    // One way of distinguishing between different types of messages sent from
-    // your HTML page is to use an object with a "type" property like this.
+
+    // If the message is 'create-squiggle' then run the squiggle building
+    // Leaving this as a conditional becuase I plan to add other message types in the future
     if (msg.type === "create-squiggles") {
         const nodes = [];
-        console.log(msg.red);
+
+        // Run this loop i times where i is equal to the user count input
         for (let i = 0; i < msg.count; i++) {
+
             // ---------------------
             // BEGIN SVG BUILDER
             // ---------------------
+            // This array is going to hold the entire set of coords for each individual squiggle
             let array = [];
             let multValue = 1000;
+            // Interpret complexity form user complexity input
             let complexity = msg.comp;
-            // This builds the initial marker down coordinates
+            // This pushes the initial marker down into the array for the SVG coordinates
             array.push("M");
+            // This build a random set of coordinates for where the initial marker down should occur
             for (let m = 0; m < 2; m++) {
                 let rando = Math.random() * multValue;
                 array.push(rando);
             }
-            // This builds a set of curve coordinates
+            // This builds a set of curve coordinates x times where x is equal to user complexity input
             for (let x = 0; x < complexity; x++) {
+                // This pushes the C into the array for curves in the SVG coordinates
                 array.push("C");
+                // The are 3 sets of coordinates in an SVG curve 
+                // The first two coordinates are where the point lands 
+                // The second two sets are each the coordinates of where the bezier handles should land
                 for (let c = 0; c < 6; c++) {
                     let rando = Math.random() * multValue;
                     array.push(rando);
                 }
             }
+
+            // joinArray brings all of the coordinates together, removing commas and adding a space between index values
             let joinArray = array.join(" ");
+            // strinArray turns joinArray into a string
             let stringArray = joinArray.toString();
-            console.log(stringArray);
             // ---------------------
             // END SVG BUILDER
             // ---------------------
             const vector = figma.createVector();
+            // This spaces the created squiggles out across the y-axis
             vector.y = i * 300;
+            // Set random opacity per squiggle
             vector.opacity = Math.random();
-            vector.strokes = [{ type: "SOLID", color: { r: 1, g: 0.196, b: 0.635 } }];
+            // Set the default stroke color
+            vector.strokes = [{ type: "SOLID", color: { r: .1568, g: 0.1568, b: 0.7412 } }];
+            // Set random stroke weight per squiggle
             vector.strokeWeight = Math.random() * 10;
             figma.currentPage.appendChild(vector);
             vector.vectorPaths = [
                 {
                     windingRule: "EVENODD",
-                    // data: "M 0 100 L 100 100 L 50 0 Z"
+                    // Set dats equal to the coords generated for the squiggle (ie joinArray) 
                     data: "" + joinArray + ""
                 }
             ];
             nodes.push(vector);
         }
+        // Select the newly created squiggles
         figma.currentPage.selection = nodes;
+        // Fit the viewport to include the select (ie the Squiggles)
         figma.viewport.scrollAndZoomIntoView(nodes);
     }
-    // Make sure to close the plugin when you're done. Otherwise the plugin will
-    // keep running, which shows the cancel button at the bottom of the screen.
+    // Close the plugin
     figma.closePlugin();
 };
